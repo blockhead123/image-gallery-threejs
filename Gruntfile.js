3dcad924698a07a5a7003ffe112576b3b81bacdc
Oliver Ong <tefiri@gmail.com>
2014-10-01 07:11:30 +0800

    added grunt and distribution structures

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        replace: {
+            example: {
+                src: ['src/index.html'],             // source files array (supports minimatch)
+                dest: 'dist/',             // destination directory or file
+                replacements: [{
+                    from: 'image-gallery-three.js',                   // string replacement
+                    to: 'image-gallery-three.min.js'
+                },{
+                    from: 'assets.js',                   // string replacement
+                    to: 'assets.min.js'
+                },{
+                    from: 'image-gallery-three.css',                   // string replacement
+                    to: 'image-gallery-three.min.css'
+                }]
+            }
+        },
+        uglify: {
+            dist: {
+                files: {
+                    'dist/js/image-gallery-three.min.js': ['src/js/image-gallery-three.js'],
+                    'dist/js/assets.min.js': ['src/js/assets.js']
+                }
+            }
+        },
+        cssmin : {
+            styles: {
+                src: ['src/css/image-gallery-three.css'],
+                dest: 'dist/css/image-gallery-three.min.css'
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-text-replace');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default', ['replace', 'uglify', 'cssmin']);
+
+};
